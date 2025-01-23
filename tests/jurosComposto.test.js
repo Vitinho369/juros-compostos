4daf647a439e4516e04026335320c760f9448c79
Vitinho369 <vitinho21.2005@gmail.com>
2025-01-22 23:52:57 -0300

    test: testando função com valores negativos

--- tests/jurosComposto.test.js
+++ tests/jurosComposto.test.js
@@ -4,3 +4,8 @@ test('calcula juros compostos com valores positivos', () => {
     const calculo = new JurosCompostos(1000, 5, 12);
     expect(calculo.calcular()).toBe(1795.86);
 });
+
+test('calcula juros compostos com taxa de juros negativa', () => {
+    const calculo = new JurosCompostos(1000, -5, 12);
+    expect(calculo.calcular()).toBe(552.29);
+});
