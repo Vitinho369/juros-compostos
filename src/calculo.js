ce6aad5e5ee5846fcf5c9805db173814d6a2588f
Vitinho369 <vitinho21.2005@gmail.com>
2025-01-22 23:42:48 -0300

    refactor: aplicando orientação objeto ao sistema

--- src/calculo.js
+++ src/calculo.js
@@ -1,4 +1,23 @@
-const calcular = (capitalInicial, taxaJuros, periodos) => {
-    const montante = capitalInicial * Math.pow((1 + taxaJuros), periodos);
-    return parseFloat(montante.toFixed(2));
+class JurosCompostos {
+    constructor(capitalInicial, taxaJuros, periodos) {
+        this.capitalInicial = capitalInicial;
+        this.taxaJuros = taxaJuros / 100;
+        this.periodos = periodos;
+    }
+
+    calcular() {
+        const montante = this.capitalInicial * Math.pow((1 + this.taxaJuros), this.periodos);
+        return parseFloat(montante.toFixed(2));
+    }
+
+    gerarRelatorio() {
+        const montante = this.calcular();
+        return (
+            `Capital Inicial: R$ ${this.capitalInicial}\n` +
+            `Taxa de Juros: ${(this.taxaJuros * 100).toFixed(2)}%\n` +
+            `Períodos: ${this.periodos}\n` +
+            `Montante Acumulado: R$ ${montante}`
+        );
+    }
 }
+
